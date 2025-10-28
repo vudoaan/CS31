6f8262d87b31fd8cd087abe62d03b8ed9a7b709a
Vu Doan <vd626636@gmail.com>
2025-10-28 04:16:29 -0700

    I THINK IM DONE???

--- Project 3/dance.cpp
+++ Project 3/dance.cpp
@@ -22,12 +22,11 @@ bool isvalidDirection(char direction) {
 
 bool isSyntacticallyCorrect(std::string dance) {
     //Count for the index where we're at in the string
-    std::size_t i = 0;
-    std:: size_t stringSize = dance.size();
+    std::size_t i = 0, stringSize = dance.size();
     //Checks if string is empty
-    if (dance.size() == 0)
+    if (stringSize == 0)
         return true;
-    while (i < dance.size())  {
+    while (i < stringSize)  {
         //Checks if string at index i is a valid direction, digit, or beat, return false if it isn't
         if (!isvalidDirection(dance.at(i)) && !isdigit(dance.at(i)) && dance.at(i) != '/') {
             return false;
@@ -83,17 +82,100 @@ bool isSyntacticallyCorrect(std::string dance) {
     return true;
 }
 
-/*int translateDance(std::string dance, std::string& instructions, int& badBeat) {
-    int beatCount = 0;
+int translateDance(std::string dance, std::string& instructions, int& badBeat) {
+    std::string originalInstructions = instructions;
+    instructions = "";
+    std::size_t i = 0, stringSize = dance.size();
+    int freezeCount = 0, freezeAmount = 0, beatCount = 0;
+    char direction;
+     
+    //Checks if the dance is syntactically correct using the isSyntacticallyCorrect() function, if false, return 1
+    if (isSyntacticallyCorrect(dance) == false)
+        return 1;
+    while (i < stringSize) {
+        freezeCount = 0, freezeAmount = 0;
+        std::cerr << "In loop" << std::endl;
+        if (isdigit(dance.at(i))) {
+            std::cerr << "1st digit" << std::endl;
+            if (isdigit(dance.at(i + 1))) {
+                std::cerr << "2nd digit" << std::endl;
+                freezeAmount = ((dance.at(i) - '0') * 10) + (dance.at(i + 1) - '0');
+                direction = dance.at(i + 2);
+                if (freezeAmount < 2) {
+                    instructions = originalInstructions;
+                    badBeat = beatCount + 1;
+                    return 2;
+                }
+                i += 3;
+                for (int j = i; j < freezeAmount + i && j < stringSize; j++) {
+                    std::cerr << j << ":" << dance.at(j) << std::endl;
+                    instructions += toupper(direction);
+                    if (dance.at(j) != '/') {
+                        instructions = originalInstructions;
+                        badBeat = beatCount + 1; 
+                        return 3;
+                    }
+                    beatCount++;
+                    freezeCount++;
+                }
+                std::cerr << i << std::endl;
+                std::cerr << freezeCount << freezeAmount << std::endl;
+                if (freezeCount != freezeAmount) {
+                    instructions = originalInstructions;
+                    badBeat = beatCount + 1;
+                    return 4;
+                } else if(i != stringSize) {
+                    i+= freezeAmount;
+                }
+            } else {
+                std::cerr << "Only one digit" << std::endl;
+                freezeAmount = dance.at(i) - '0';
+                direction = dance.at(i + 1);
+                if (freezeAmount < 2) {
+                    instructions = originalInstructions;
+                    badBeat = beatCount + 1;
+                    return 2;
+                }
+                i+= 2;
+                for (int j = i; j < freezeAmount + i && j < stringSize; j++) {
+                    instructions += toupper(direction);
+                    if (dance.at(j) != '/') {
+                        instructions = originalInstructions;
+                        badBeat = beatCount + 1; 
+                        return 3;
+                    }
+                    beatCount++;
+                    freezeCount++;
+                }
+                if (freezeCount != freezeAmount) {
+                    instructions = originalInstructions;
+                    badBeat = beatCount + 1;
+                    return 4;
+                } else if(i != stringSize) {
+                    i+= freezeAmount;
+                }
+            }
+        } else if (isvalidDirection(dance.at(i))) {
+            instructions += tolower(dance.at(i));
+            beatCount++;
+            i += 2;
+        } else {
+            instructions += '.';
+            i++;
+        }
+        std::cerr << "Index at end: " << i << std::endl;
+    }
+    //The string dance is translateable and returns 0
     return 0;
-    return 1;
-    return 2;
-    return 3;
-    return 4;
-} */
+}
 
 int main()
     {
-        std::cerr << "Hello" << std::endl;
-        std::cerr << std::boolalpha <<isSyntacticallyCorrect("u/3r///3d///") << std::endl;
+        std::string instructions = "errorrrrrr";
+        int badBeat = -999;
+        int returnCode = translateDance("d/3r//u/0d//2u/", instructions, badBeat);
+        //std::cerr << std::boolalpha << isSyntacticallyCorrect("10r//////////") << std::endl;
+        std::cerr << "Return Code: " << returnCode << std::endl << "Instructions: " << instructions << std::endl << "Bad Beat: " << badBeat << std::endl;
+        //std::cerr << ('3' - '0') * 10 + ('3' - '0') << std::endl;
+        return 0;
     }
