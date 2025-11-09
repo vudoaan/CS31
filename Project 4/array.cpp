f7845af0548d07440209f03633ac742aef0c6ea3
Vu Doan <vd626636@gmail.com>
2025-11-08 20:09:38 -0800

    Removed unnecessary checks

--- Project 4/array.cpp
+++ Project 4/array.cpp
@@ -3,142 +3,185 @@
 #include <string>
 
 int repeat(std::string a[], int n) {
+    //Bad Argument Checks returns -1
+    //Checks if passed arguement n is a negative array size
     if (n < 0) {
         return -1;
     }
-    
+    //Loops through array and appends the element to itself to get a twice repeated string
     for (int i = 0; i < n; i++) {
         a[i] += a[i]; 
     }
+    //Returns the amount of indicies affected
     return n;
 }
 
 int detectMatch(const std::string a[], int n, std::string target) {
-    if (n < 0) {
+    //Bad Argument Checks returns -1
+    //Checks if passed arguement n is a negative array size
+    if (n <= 0) {
         return -1;
     }
+    //Loops through the array and checks if target is an index in it
+    //If found then return the index of the first occuring instance
+    //If not found then return -1
     for (int i = 0; i < n; i++) {
         if (a[i] == target) {
             return i;
         }
     }
+    //Returns -1 if it can't find a match for target in the array
     return -1;
 }
 
 int detectMax(const std::string a[], int n) {
-    if (n < 0) {
+    //Bad Argument Checks returns -1
+    //Checks if passed arguement n is a negative array size or examines no elements in the array
+    if (n <= 0) {
         return -1;
     }
-    int j, latest = 0;
-    for (int i = 0; i < n; i++) {
-        if (a[i][0] > a[latest][0]) {
+    //Sets the starting string comparison to the first string
+    int latest = 0;
+    //Loops through array
+    for (int i = 1; i < n; i++) {
+        //If string of array[i] is bigger array[latest] then set i to latest
+        if (a[i] > a[latest]) {
             latest = i;
-        } else if (a[i][0] == a[latest][0]) {
-            for (j = 0; j < a[i].length() && a[latest].length(); j++) {
-                if (a[i][j] != a[latest][j]) {
-                    break;
-                }
-            }
-            if (j == a[i].length() || j == a[latest].length()) {
-                if (a[i].length() > a[latest].length()) {
-                    latest = i;
-                }
-            } else if (a[i][j] > a[latest][j]) {
-                latest = i;
-            }
         }
     }
+    //Return the biggest string alphabetically
     return latest;
 }
-//Questions
-//What if pos is 1 and n is 4 but the array size is bigger than 4? Will we still have to change the rest of the array?
+
 //Extra Credit Question (No extra array)
 int circleLeft(std::string a[], int n, int pos) {
-    if (n < 0 || pos >= n || pos < 0) {
+    //Bad Argument Checks returns -1
+    //Checks if passed arguement n is a negative array size or examines no elements in the array
+    //Checks if passed argument position than array size
+    //Checks if passed arguement positiion is negative
+    if (n <= 0 || pos >= n || pos < 0) {
         return -1;
     }
+    //Creates the temporary string at array[pos] so that it can be inserted later after everything is circled left
     std::string temp = a[pos];
-    for (int i = 0; i < n; i++) { 
-        if (i >= pos && i < n - 1) {
-            a[i] = a[i + 1];
-        }
+    //Loops through the array and when it hits the position that was eliminated, start moving eveything to the left
+    //Stops moving left at i - 1 to leave room for the eliminated string at array[pos]
+    for (int i = pos; i < n - 1; i++) { 
+        a[i] = a[i + 1];
     }
+    //After everything is moved, replace the last index with string at array[pos]
     a[n - 1] = temp;
+    //Return the position where string at array[pos] was elimnated
     return pos;
 }
 
 int enumerateRuns(const std::string a[], int n) {
+    //Bad Argument Checks returns -1
+    //Checks if passed arguement n is a negative array size
     if (n < 0) {
         return -1;
+    //Checks if no elements are examined in the array, if so return so because no runs are in an empty sequence
+    } else if (n == 0) {
+        return 0;
     }
+    //Makes the first sequence the first index of the string
     std::string currentString = a[0];
+    //Counter for sequences in the array
     int counter = 1;
+    //Loops through the array and checks if the next string is identical
+    //If not identical then make the new sequence the stirng at array[i] and add one the counter as there is a new sequence
     for (int i = 1; i < n; i++) {
         if (a[i] != currentString) {
             currentString = a[i];
             counter++;
         }
     }
+    //Returns the count of sequences in the array
     return counter;
 }
 
 //Extra Credit Question (No extra array)
 int flip(std::string a[], int n) {
+    //Bad Argument Checks returns -1
+    //Checks if passed arguement n is a negative array size
     if (n < 0) {
         return -1;
     }
     std::string temp;
+    //Loops through array and flips elements
+    //Store current string to temp, set the current string to opposing indexed string, and then set the flipped string to the stored string
+    //Stops at n/2 because you only need to half of the array for it to be flipped and if odd indexed, middle string doesn't need to be flipped
     for (int i = 0; i < n/2; i++) {
         temp = a[i];
         a[i] = a[n - i - 1];
         a[n - i - 1] = temp;
     }
+    //Returns the number of elements flipped
     return n;
 }
 
 int detectDifference(const std::string a1[], int n1, const std::string a2[], int n2) {
+    //Bad Argument Checks returns -1
+    //Checks if passed arguements n1 or n2 are negative array sizes
     if (n1 < 0 || n2 < 0) {
         return -1;
     }
+    //Loops through the array and checks if the theres a difference between the the indicies
+    //If there is return i because that's where the difference starts
     for (int i = 0; i < n1 && i < n2; i++) {
         if (a1[i] != a2[i]) {
             return i;
         }
     }
+    //Checks which array ends first and returns the shorter array size
+    //If n1 ended first return n1 or both ended at the same time return n1
+    //If n2 ended first return n2
     if (n1 <= n2) {
         return n1;
     } else {
         return n2;
     }  
 }
-//Questions
-//Can n1 ever be smaller than n2?
-//What does it mean to consider a sequence of 0 elements?
+
 int subsequence(const std::string a1[], int n1, const std::string a2[], int n2) {
+    //Bad Argument Checks returns -1
+    //Checks if passed arguement n1 and n2 are negative array sizes
+    //Checks if n2 is bigger than n1 because thats not possible
     if (n1 < 0 || n2 < 0 || n1 < n2) {
         return -1;
+    //If n2 == 0 then the subsequence starts at the first element so return 0
     } else if (n2 == 0) {
         return 0;
     }
     int j;
+    //Loops through the array and checks if all elements in n2 appear in n1
     for (int i = 0; i < n1; i++) {
+        //Checks for the starting element of n2 in n1
+        //If there is continue
         if (a2[0] == a1[i]) {
+            //Checks for the rest of the elements of n2 in n1
+            //Bounds of the length of n1 and n2, whichever comes first
+            //If there is a different element break from the loop
             for (j = 1; j < n2 && i + j < n1; j++) {
                 if (a2[j] != a1[i+j]) {
                     break;
                 }
             }
+            //Checks if the amount of elements in n2 is in n1
+            //If so return the index where ti starts
             if (j == n2) {
                 return i;
             }
         }
     }
+    //Returns -1 if it doesn't find a subsequence
     return -1;
 }
-//Questions
-//What if both strings have no elements?
+
 int detectAny(const std::string a1[], int n1, const std::string a2[], int n2) {
-    if (n1 < 0 || n2 < 0) {
+    //Bad Argument Checks returns -1
+    //Checks if passed arguement n1 and n2 are negative array sizes
+    if (n1 < 0 || n2 <= 0) {
         return -1;
     }
     for (int i = 0; i < n1; i++) {
@@ -151,66 +194,34 @@ int detectAny(const std::string a1[], int n1, const std::string a2[], int n2) {
     return -1;
 }
 //Questions
-//Do we need to account for the alphabetical after the first letter of splitter?
 //Extra Credit Question (No extra array)
 //Alphabetical Question
 int split(std::string a[], int n, std::string splitter) {
+    //Bad Argument Checks returns -1
+    //Checks if passed arguement n is a negative array size
+    //Checks if passed argument n is 0, you split 0 strings so the split starts at 0
     if (n < 0) {
         return -1;
+    } else if (n == 0) {
+        return 0;
     }
+    //Temp variable to swap
     std::string temp;
-    int counter, k, l;
-    //Sorts alphabetically
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (a[i][0] > a[j][0]) {
-                temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
-            } else if (a[i][0] == a[j][0]) {
-                for (k = 1; k < a[i].length() && k < a[j].length(); k++) {
-                    if (a[i][k] != a[j][k]) { 
-                        break;
-                    }
-                }
-                if (k == a[i].length() || k == a[j].length()){
-                    if (a[i].length() > a[j].length()){
-                        temp = a[i];
-                        a[i] = a[j];
-                        a[j] = temp;
-                    }
-                } else if (a[i][k] > a[j][k]) {
-                    temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
-                } 
-            }
+    //counter keeps track of the index at which the split starts
+    int counter = 0;
+    //Loops through the array and sees the first index that is not < splitter
+    for (int i = 0; i < n ;i++) {
+        if (a[i] < splitter) {
+            temp = a[i];
+            a[i] = a[counter];
+            a[counter] = temp;
+            counter++;
         }
     }
-    for (counter = 0; counter < n; counter++) {
-        if (a[counter] == splitter) {
-            break;
-        } else if (a[counter][0] == splitter[0]){
-            for (l = 0; l < a[counter].length() && splitter.length(); l++) {
-                if(a[counter][l] != splitter[l]) {
-                    break;
-                }
-            }
-            if (l == a[counter].length() || l == splitter.length()){
-                if (a[counter].length() > splitter.length()){
-                    break;
-                }
-            } else if (a[counter][l] > splitter[l]) {
-                break;
-            } 
-        } else if (a[counter][0] > splitter[0]) {
-            break;
-        }
-    }
+    //Return where the split started
     return counter;
 }
-//Questions
-//
+
 int main () {
     //Test for function repeat
     std::cerr << '\n' << "Test for function repeat\n---\n";
